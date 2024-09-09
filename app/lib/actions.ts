7b4bed5af84d743997c27f1d02f6f16e9c28516c
Morten Jakobsen <Mojak18@student.sdu.dk>
2024-09-09 18:53:12 +0200

    added delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -93,7 +93,7 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Not Implemented");
+  // throw new Error("Not Implemented");
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (error) {
